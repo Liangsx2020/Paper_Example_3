defbd5635c2b295db89bff21ee5a5e8e730443b4
Shuxuan Liang <liang.shuxuan2024@gmail.com>
2025-11-10 12:36:29 +0900

    ex3 finished

--- DeepNitsche.py
+++ DeepNitsche.py
@@ -437,7 +437,7 @@ def loss(model, X_inner, weights, Rf_inner, X_bd, U_bd, X_interface, Normal_inte
     return total_loss
 
 # Number of grid points per quadrant
-quarant_number_one_side = 256 # 16, 24, 32, 48, 64
+quarant_number_one_side = 128 # 16, 24, 32, 48, 64
 
 N_inner_quadrant = 1 
 # number of grid points
